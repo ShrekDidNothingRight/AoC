66b3e176f03afa18fd1d699c2bdad55c2d07738d
ShrekDidNothingRight <95369249+ShrekDidNothingRight@users.noreply.github.com>
2021-12-02 13:02:30 +0000

    Update AOC1_1.py

--- AOC1_1.py
+++ AOC1_1.py
@@ -1,11 +1,8 @@
 file = open(r"C:\Users\alt\desktop\aoc\data.txt")
-
-#compare = lines.toint[+1]
-
-#print("the original list is: " + str(file.readlines()))
-
 sub_data = list(map(int, file.readlines()))
 count = 0
+
+
 for i in range(len(sub_data)):
     if sub_data[i] > sub_data[i - 1]:
         count = count + 1
